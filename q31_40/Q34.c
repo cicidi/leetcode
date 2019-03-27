80d3a940ee48b1843950fc2c83abe788202aceb4
Walter Chen <walter.chen@byton.com>
2019-03-27 01:00:15 -0700

    practice

---
+++ q31_40/Q34.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   combinationSum(1,2,3,4,5);
+
+
+   return 0;
+}
+
+void combinationSum(int candidates, int candidatesSize, int target, int columnSizes, int returnSize) {
+printf("get numberaa");
+}
