55e3cf60d1e6cb5cec050a915045f5908706e6ae
cicidi <cicidi@gmail.com>
2019-10-25 17:47:11 -0700

    git ignore

---
+++ algorithm/company/Cruise/SpacseMatrix.java
@@ -0,0 +1,9 @@
+package company.Cruise;
+
+/**
+ * @author walter on 2019-10-22
+ * Lintcode
+ * url
+ */
+public class SpacseMatrix {
+}
---
+++ algorithm/company/carta/Report.java
@@ -0,0 +1,9 @@
+package company.carta;
+
+/**
+ * @author walter on 2019-10-24
+ * Lintcode
+ * url
+ */
+public class Report {
+}
