127c42fb02b9afc5b9160f91600707baf1358746
cicidi <cicidi@gmail.com>
2020-01-20 19:22:09 -0800

    move complete to archieve

--- algorithm/tree/SymmetricTree.java
+++ algorithm/tree/SymmetricTree.java
@@ -1,4 +1,4 @@
-/*
+package tree;/*
  * @lc app=leetcode id=101 lang=java
  *
  * [101] Symmetric Tree
@@ -13,60 +13,63 @@
  *
  * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
  * around its center).
- * 
+ *
  * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
- * 
- * 
+ *
+ *
  * ⁠   1
  * ⁠  / \
  * ⁠ 2   2
  * ⁠/ \ / \
  * 3  4 4  3
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * But the following [1,2,2,null,3,null,3] is not:
- * 
- * 
+ *
+ *
  * ⁠   1
  * ⁠  / \
  * ⁠ 2   2
  * ⁠  \   \
  * ⁠  3    3
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Note:
  * Bonus points if you could solve it both recursively and iteratively.
- * 
+ *
  */
+
+import model.TreeNode;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
-            if (root == null) 
-                return true;
-            return isSym(root.left,root.right);
+        if (root == null)
+            return true;
+        return isSym(root.left, root.right);
     }
 
-    public boolean isSym(TreeNode n1, TreeNode n2){
-        if (n1 == null && n2 == null){
+    public boolean isSym(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) {
             return true;
         }
-        if (n1!= null && n2!= null){
-            if (n1.val != n2.val){
+        if (n1 != null && n2 != null) {
+            if (n1.val != n2.val) {
                 return false;
-            }else{
-                return n1.val == n2.val && isSym(n1.left, n2.right) && isSym(n1.right,n2.left);
+            } else {
+                return n1.val == n2.val && isSym(n1.left, n2.right) && isSym(n1.right, n2.left);
             }
         }
         return false;
