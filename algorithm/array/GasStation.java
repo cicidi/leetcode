4ed985b931ad2e5cb3448f07ff94b549ca4b66fa
cicidi <cicidi@gmail.com>
2020-03-29 02:37:49 -0700

    add bad but working solutionwq

--- algorithm/array/GasStation.java
+++ algorithm/array/GasStation.java
@@ -20,15 +20,22 @@ Travel to station 1. Your tank = 7 - 3 + 2 = 6
 Travel to station 2. Your tank = 6 - 4 + 3 = 5
 Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
 Therefore, return 3 as the starting index.*
+
  * 分析
+ *
+ * 1. your tank = 1 - 3 failed
+ * 2. your tank = 2 - 4 failed
+ * 3. your tank = 3 -5 failed
+ * 4. your ...
+ * 5. your tank = 5 - 2 = 3 
+ *                3 + 1 - 3 = 1
+ *                 1 + 2 - 4  no  falied
+ *                 
  * */
 
 public class GasStation {
 
     public static void main(String[] args) {
-//        String[] input = new String[]{
-//                "wrt", "wrf", "er", "ett", "rftt"
-//        };
         Solution s = new Solution();
         int[] gas = {1, 2, 3, 4, 5};
         int[] cost = {3, 4, 5, 1, 2};
@@ -48,8 +55,10 @@ public class GasStation {
             int index = -1;
 
             for (int i = 0; i < gas.length; i++) {
-                sum += gas[i] - cost[i];  //notice 一个用来track 当前 sum  如果sum 小于0  index走下一个, sum重新开始
-                total += gas[i] - cost[i]; // 用一个total 来track
+                //notice 一个用来track 当前 sum  如果sum 小于0  index走下一个, sum重新开始
+                sum += gas[i] - cost[i];  
+                // 用一个total 来track
+                total += gas[i] - cost[i]; 
                 if (sum < 0) {
                     index = i;
                     sum = 0;
@@ -58,6 +67,39 @@ public class GasStation {
             return total < 0 ? -1 : index + 1;
             // index should be updated here for cases ([5], [4]);
             // total < 0 is for case [2], [2]
+        }
+        public int canCompleteCircuitSolution2(int[] gas, int[] cost) {
+            if (gas == null || cost == null || gas.length <= 0 || cost.length == 0){
+                return -1;
+            }
+        
+            int total = 0;
+            int cur = 0;
+            int index = -1;
+            for (int i = 0; i < gas.length; i++){
+                int count = 0;
+                for (int j = i; count < gas.length; count++) {
+                    cur = cur + gas[j] - cost[j];
+                    // if cur < 0 then start from next i
+                    if (cur < 0){
+                        cur = 0;
+                        total = 0;
+                        index = -1;
+                        break;
+                    }
+                    if (j + 1 == gas.length){
+                        j = 0;
+                    }else{
+                        j = j + 1;
+                    }
+                    index = j;
+                }
+                if (cur >= 0 && index != -1){
+                    System.out.println(" stop " + i);
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
